135ff9920b136ef497c83fbb155696a7a4e488f9
Garoui <118277240+Garoui@users.noreply.github.com>
2025-03-20 21:05:35 +0100

    table formateur et apprenant

--- src/views/admin/Tables.js
+++ src/views/admin/Tables.js
@@ -2,14 +2,19 @@ import React from "react";
 
 // components
 
-import CardTable from "components/Cards/CardTable.js";
+import CardTableListFormateurs from "components/Cards/CardTableListFormateurs";
+import CardTableListUsers from "components/Cards/CardTableListUsers.js";
+
 
 export default function Tables() {
   return (
     <>
       <div className="flex flex-wrap mt-4">
         <div className="w-full mb-12 px-4">
-          <CardTable color="dark" />
+        <CardTableListFormateurs />
+          </div>
+        <div className="w-full mb-12 px-4">
+          <CardTableListUsers color="dark" />
         </div>
       </div>
     </>
